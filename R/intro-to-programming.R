d74cf1e13edd3886c6f31481577fb2a86521314e
rzahabi <zahabi@nceas.ucsb.edu>
2023-04-05 11:20:17 -0700

    initial commit

---
+++ R/intro-to-programming.R
@@ -0,0 +1,34 @@
+# r calculation with objects---
+weight_kg <- 55
+
+# convert from kg to lb
+weight_kg * 2.2
+
+weight_kg <- c(55, 25, 12)
+
+weight_kg * 2.2
+
+# data types ----
+
+science_rocks <- "yes it does!
+
+@ changing data types ---
+year <-2005
+
+year <- as.factor(year)
+
+
+
+# using read.csv () function ----
+
+
+year <- as.factor (year)
+
+# using read.csv() function ----
+big_chem_dat <- read.csv(file = "data/BGchem2008data.csv
+
+
+bg_chem_dat$Date
+
+
+
